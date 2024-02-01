4babb00fb9a7dc686ce5335374e1a4a3fee4c8c7
YunsBob <w124564@naver.com>
2024-02-01 21:13:25 +0900

    add: 자료형 공부

---
+++ study/src/main/kotlin/typeTest.kt
@@ -0,0 +1,22 @@
+const val num = 99
+
+fun main() {
+    var str: String = "Hello"
+    println(str + "World!")
+    println("$str World!")
+
+    var a: Int = 10
+    var b: Int = 20
+    println("10 + 20 = " + (a + b))
+    println("$a + $b = ${a + b}")
+
+    var c = 20.5 // 자동으로 실수형으로 받음
+    // c = 20 <- 실수형이라 불가능
+    println("$c")
+
+    // num = 30 <- 상수라 불가능
+    print("$num \n")
+
+    str = "10"
+    println("$a + $str = ${a + str.toInt()}")
+}
